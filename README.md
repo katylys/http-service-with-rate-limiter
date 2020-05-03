3b7f60ffc148872ae963854963a87fbba3b1da4a
Ekaterina Lysychenko <eyulysychenko@miem.hse.ru>
2020-05-03 20:07:28 +0300

    добавила первую версию сервиса

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # http-service-with-rate-limiter
 HTTP-сервис, способный ограничивать количество запросов (rate limit) из одной подсети IPv4. Если ограничения отсутствуют, то нужно выдавать одинаковый статический контент.
+
+запускать с параметрами prefix_subnet delay limit
+Например:
+--prefix_subnet=24 --delay=120 --limit="2 per 10 second"
---
+++ flaskk.py
@@ -0,0 +1,84 @@
+import argparse
+import ipaddress
+import socket
+import struct
+import sys
+from functools import wraps
+from flask import Flask, request, make_response, render_template
+from flask_limiter import Limiter
+import time
+
+
+class ExtLimiter(Limiter):
+    def limit_and_check(self, limiter, delay=0, limit=0):
+        def inner(func):
+            limiter.limit(self)(func)
+            flag = 1  # rate limit не превышен
+            start = 0
+            @wraps(func)
+            def check(*args, **kwargs):
+                if not limit:
+                    return func(*args, **kwargs)
+                nonlocal flag
+                nonlocal start
+                if flag:
+                    try:
+                        limiter.check()
+                    except:
+                        start = time.time()
+                        flag = 0
+                if not flag:
+                    diff = time.time() - start
+                    if diff < delay:
+                        pieces = limit.split(" ")
+                        number_requests = pieces[0]
+                        end_req = "" if number_requests == "1" else "s"
+                        req = "request" + end_req
+                        pieces.insert(1, req)
+                        paste_limit = " ".join(pieces)
+                        resp = make_response(render_template('429.html', LIMIT=paste_limit), 429)
+                        resp.headers['Content-Type'] = 'text/html'
+                        resp.headers['Retry-After'] = delay - int(diff)
+                        return resp
+                    else:
+                        flag = 1
+                        start = 0
+                return func(*args, **kwargs)
+            return check
+        return inner
+
+
+def get_subnet(subnet):
+    def inner():
+        net = ipaddress.ip_network(request.remote_addr + "/" + subnet, strict=False)
+        return str(net.network_address)
+    return inner
+
+
+def main(limit, prefix_subnet, delay):
+    mask = (1 << 32) - (1 << 32 >> int(prefix_subnet))
+    mask_subnet = socket.inet_ntoa(struct.pack(">L", mask))
+    app = Flask(__name__, template_folder='./templates')
+    limiter = ExtLimiter(app, default_limits=[limit], key_func=get_subnet(mask_subnet), strategy="moving-window")
+
+    @app.route('/')
+    @limiter.limit_and_check(limiter, delay=int(delay), limit=limit)
+    def index():
+        return 'INDEX'
+    app.run(debug=True)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--limit', default=0)
+    parser.add_argument('-p', '--prefix_subnet')
+    parser.add_argument('-d', '--delay')
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    main(namespace.limit, namespace.prefix_subnet, namespace.delay)
